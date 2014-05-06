c83b4688bada64c60a086d963994c4c6f956617e
Alex Sink <alexsink@alexsink.org>
2014-05-06 13:52:27 -0700

    remove dead code

--- StrangeRocks/Assets/Transfluent/Editor/AssetScanner.cs
+++ StrangeRocks/Assets/Transfluent/Editor/AssetScanner.cs
@@ -10,6 +10,17 @@ namespace transfluent
 {
 	public class AssetScanner
 	{
+		private readonly List<GameSpecificMigration.IGameProcessor> _gameProcessors =
+			new List<GameSpecificMigration.IGameProcessor>();
+		List<GameObject> toIgnore = new List<GameObject>();
+		private readonly GameSpecificMigration.CustomScriptProcessorState _customProcessorState;
+		public AssetScanner()
+		{
+			_customProcessorState = new GameSpecificMigration.CustomScriptProcessorState(toIgnore, TranslationUtility.getUtilityInstanceForDebugging());
+			_gameProcessors.Add(new GameSpecificMigration.ButtonViewProcessor());
+			_gameProcessors.Add(new GameSpecificMigration.TextMeshProcessor());
+		}
+
 		[MenuItem("Translation/testScan")]
 		public static void testscan()
 		{
@@ -21,27 +32,15 @@ namespace transfluent
 			}*/
 			//scanner.searchGameObjects(scanner.getAllGameObjectsInScene());
 
-			string activeSelectionPath = AssetDatabase.GetAssetPath(Selection.activeObject);
-			activeSelectionPath = "Assets/art/scenes/level_block.prefab";
-			Debug.Log("Active selection path:" + activeSelectionPath);
+			//string activeSelectionPath = AssetDatabase.GetAssetPath(Selection.activeObject);
+			//activeSelectionPath = "Assets/art/scenes/level_block.prefab";
+			//Debug.Log("Active selection path:" + activeSelectionPath);
 			//scanner.searchPrefab(activeSelectionPath);
 			//scanner.searchGameObjects();
-
 
 			scanner.searchScenes();
 
 			scanner.searchPrefabs();
-		}
-
-		private readonly List<GameSpecificMigration.IGameProcessor> _gameProcessors =
-			new List<GameSpecificMigration.IGameProcessor>();
-		List<GameObject> toIgnore = new List<GameObject>();
-		private readonly GameSpecificMigration.CustomScriptProcessorState _customProcessorState;
-		public AssetScanner()
-		{
-			_customProcessorState = new GameSpecificMigration.CustomScriptProcessorState(toIgnore, TranslationUtility.getUtilityInstanceForDebugging());
-			_gameProcessors.Add(new GameSpecificMigration.ButtonViewProcessor());
-			_gameProcessors.Add(new GameSpecificMigration.TextMeshProcessor());
 		}
 
 		public void searchPrefabs()
@@ -133,46 +132,6 @@ namespace transfluent
 			}
 		}
 		
-#if false
-		public List<GameObject> getAllGameObjectsInPrefab(GameObject prefab)
-		{
-			var originalScene = EditorApplication.currentScene;
-			//cheat with a scene in order to make sure to get disabled components.  Feels odd, but I hope I find something better later
-			EditorApplication.NewScene();
-
-			//delete the main camera and any other default objects
-			var gameobjects = GameObject.FindObjectsOfType<GameObject>();
-			foreach(var existingGo in gameobjects)
-			{
-				GameObject.DestroyImmediate(existingGo);
-			}
-
-			GameObject go = GameObject.Instantiate(prefab) as GameObject;
-
-			var listOfGameobjectsInPrefab = getAllGameObjectsInScene();
-			PrefabUtility.ReplacePrefab(go,prefab)
-			//GameObject.DestroyImmediate(go);
-
-			string scene = originalScene.Replace("Assets/", "");
-			//EditorApplication.OpenScene(scene); //go back to the scene we started with
-
-			return listOfGameobjectsInPrefab;
-		}
-#endif
-		public List<GameObject> getAllGameObjectsInPrefabOldWay(GameObject prefab)
-		{
-			Component[] allcomponents = prefab.GetComponentsInChildren<Component>(true);
-			var allGameobjectsWithThing = new List<GameObject>();
-			foreach(var comp in allcomponents)
-			{
-				if(!allGameobjectsWithThing.Contains(comp.gameObject))
-				{
-					allGameobjectsWithThing.Add(comp.gameObject);
-				}
-			}
-			return allGameobjectsWithThing;
-		}
-
 		//finding disabled objects
 		//http://docs.unity3d.com/Documentation/ScriptReference/Resources.FindObjectsOfTypeAll.html
 		public List<GameObject> getAllGameObjectsInScene()
